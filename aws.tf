4e1604c2eb978b1338ac1dba4b4200e6020459ee
greedyvariables <nidzg1md@gmail.com>
2019-01-25 13:35:07 -0500

    change to aws.tf variables added so they will look at terraform.tfvars

--- aws.tf
+++ aws.tf
@@ -2,5 +2,7 @@
 # This sets providor and stop region prompts
 provider "aws" {
   region = "us-east-1"
-  skip_region_validation      = true
+  skip_region_validation  = true
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
